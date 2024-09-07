41a6834438dadf52dd143220b421438bab0156db
leesanha <ask1053@naver.com>
2024-09-07 21:56:18 +0900

    second commit

--- 0907.c
+++ 0907.c
@@ -8,3 +8,23 @@ int main ()
 
     return 0;
 }
+
+// . means 전부 다 
+// git add 어떤 파일을 추가해줄지 찾아보겠다.
+// --> git add . means 여기 프로젝트 폴더 전부를 올리겠다.
+//  ex) git add index html (html만 올리고 싶다.)
+
+// git status means 너가 추가해놓은 파일들이 뭔지 현재 상태 알려줌
+
+/*
+
+git commit -m "first commit" 
+
+git commit : 히스토리를 만들어주는것.
+"first commit" : 그 히스토리의 이름. 
+
+--> 이 히스토리를 깃 허브에 보낼것이다. 어떻게 보내냐? 연결고리가 필요하다. 
+
+
+github.com/GitHub-ux-0714/TIL
+*/
